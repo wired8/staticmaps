871ee48c4d184207fbb9336ebca52973e74ff23a
Phil Jepsen <phil@wired8.com>
2018-12-17 16:37:18 +1300

    Updated array min/max prototype

--- dist/helper/helper.js
+++ dist/helper/helper.js
@@ -1,13 +1,8 @@
 "use strict";
 
-Object.defineProperty(Array.prototype, "max", {
-  value: function max() {
-    return Math.min.apply(null, this);
-  }
-});
-
-Object.defineProperty(Array.prototype, "min", {
-  value: function min() {
-    return Math.min.apply(null, this);
-  }
-});
+Array.prototype.StaticMax = function () {
+  return Math.max.apply(null, this);
+};
+Array.prototype.StaticMin = function () {
+  return Math.min.apply(null, this);
+};
